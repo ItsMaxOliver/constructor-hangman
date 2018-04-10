f331db01481ddf4e03dcb9ec960cc05f4104e038
MollyLJohnson <mollyl.johnson@att.net>
2018-04-10 18:51:06 -0500

    restarting the build of the constructors

--- letter.js
+++ letter.js
@@ -2,33 +2,7 @@ class Letter {
     constructor(char) {
         this.char = char;
         
-        this.charCheck = function(input) {
-            if(input === char) {
-                return console.log(char);
-            }
-            else {
-                return console.log("_");
-            }
-        }
-        this.returnChar = function() {
-            if (this.charCheck) {
-                return char;
-                
-            }
-            else {
-                return "_";
-                
-            }
-        }
     }
 }
-//--------TESTING----------
-//var b = new Letter("b");
-//var f = new Letter("f");
-//var u = new Letter("u");
-//var n = new Letter("n");
-//
-//n.charCheck("b");
-
 
 module.exports = Letter;
--- word.js
+++ word.js
@@ -1,34 +1,9 @@
 var Letter = require("./letter.js");
 
-var f = new Letter("f");
-var u = new Letter("u");
-var n = new Letter("n");
-
 class Word {
     constructor() {
-        this.characters = [];
         
-        this.isGuessed = function(input) {
-            this.characters.forEach(item => {
-                return item.charCheck(input);
-            })
-        }
-        
-        this.returnWord = function() {
-            var word = [];
-            this.characters.forEach(item => {
-                word.push(item.returnChar());
-            })
-            console.log(word.join(""));
-        }
     }
 }
-//-----------TESTING---------
-var testWord = new Word();
-testWord.characters.push(f, u, n);
-
-testWord.isGuessed("u");
-testWord.isGuessed("n");
-//testWord.returnWord();
 
 module.exports = Word;
