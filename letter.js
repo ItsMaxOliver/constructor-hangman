e8b358076839f6295f58ff93f62355a165b0d6d9
MollyLJohnson <mollyl.johnson@att.net>
2018-04-10 19:54:47 -0500

    building the constructors and testing in the word.js file

--- letter.js
+++ letter.js
@@ -1,8 +1,22 @@
 class Letter {
     constructor(char) {
         this.char = char;
+        this.appear = false;
         
-    }
-}
+        this.charRender = function() {
+            if (this.char == " ") {
+                this.appear = true;
+                return " ";
+            }
+            //returns a space when it is a space
+            if (this.appear === false) {
+                return "_";
+            }
+            else {
+                return this.char;
+            }
+        };
+    };
+};
 
 module.exports = Letter;
--- word.js
+++ word.js
@@ -1,9 +1,58 @@
 var Letter = require("./letter.js");
 
 class Word {
-    constructor() {
+    constructor(word) {
+        this.word = word;
+        this.characters = [];
+        this.wordFound = false;
         
+        this.getChars = function() {
+            for (var i = 0; i < this.word.length; i++) {
+                var newLetter = new Letter(this.word[i]);
+                this.characters.push(newLetter);
+            }
+        };
+        // creates new Letter objects for each character in the word
+        
+        this.hasChar = function(input) {
+            this.characters.forEach(item => {
+                if(item.char === input) {
+                    item.appear = true;
+                }
+            })
+        };
+        // if the user input equals a letter in the word, the letter should be shown
+        
+        this.wordRender = function() {
+            var display = "";
+            this.characters.forEach(item => {
+                var currentChar = item.charRender();
+                display += currentChar;
+            });
+            return display;
+        };
+        // should return the string with each character rendered
+        
+        this.wordGuessed = function() {
+            if (this.characters.every(item => {
+                return item.appear === true;
+            }))
+            {
+                this.wordFound = true;
+                return true;
+            }
+        };
+        // checks to see if every item in the character array appears and if does, returns true so that we can generate another random word
     }
 }
+
+let amazing = new Word("amazing");
+amazing.getChars();
+console.log(amazing.wordRender());
+amazing.hasChar("z");
+console.log(amazing.wordRender());
+amazing.hasChar("a");
+console.log(amazing.wordRender());
+
 
 module.exports = Word;
