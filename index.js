bafbf5b82296351cd4a3baf11ad6e5d3010cfafe
MollyLJohnson <mollyl.johnson@att.net>
2018-10-10 21:10:12 -0500

    added some checks to functionality

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ const fs = require("fs-extra");
 const inquirer = require("inquirer");
 const chalk = require("chalk");
 let guessesLeft = 7;
-let guessedLetters = [];
-let availableChars = [];
+let guessedChars = [];
 let newWord;
 
 function generateRandomWord() {
@@ -24,16 +23,12 @@ function generateRandomWord() {
             .then( () => {           
                 promptUser();
             })
-            .catch( err => {
-                console.log(err.message);
-            })
-        .catch( err => {
-            console.log(err.message);
-        })
+            .catch( err => console.log(err.message) )
+        .catch( err => console.log(err.message) )
 }
 
 function promptUser() {
-    if (guessesLeft > 0) {
+    if ( guessesLeft > 0 ) {
         inquirer.prompt([
             {
                 name: "whichLetter",
@@ -42,42 +37,68 @@ function promptUser() {
             }
         ])
             .then( answer => {
-                //********split the answer after the first letter to make sure they are only guessing one letter at a time
-                //********check to see if the user has already guessed that letter by checking against guessedLetter array
-                //********if not proceed, other wise print message that says they already guessed that letter
-                if (newWord.hasChar(answer.whichLetter)) {
-                    console.log(chalk.green(`\nCORRECT!\n`));
-                    //******* also show a line of already guessed letters
+                let guess = answer.whichLetter[0];
+                // sets guess to the first character typed as their letter
+
+                if( guessedChars.includes(guess) ){
+                    // checks to see if the user has already guessed that letter by checking against guessedChar array
+                    console.log(chalk.red(`\nYou already guessed that letter. Try a different one.\n`));
+                    guessesLeft++;
+                    // makes it so that typing in the same letter over and over wont count against them
+                }
+                else {
+                    guessedChars.push(guess);
+                    // saves the character guessed into an array
+                }
+                /**** User guesses correctly ****/
+                if ( newWord.hasChar(guess) ) {
+                    console.log(chalk.green(`\nCorrect!\n`));
+                    console.log(chalk.cyan(`Guessed Letters: ${guessedChars}`));
+                    // shows a line of guessed chars
+                    console.log("");
                     console.log(chalk.magenta(newWord.wordRender()));
+                    // shows the underscores of word/phrase the user is trying to guess
                     console.log("");
                     newWord.wordGuessed();
+                    // runs function to see if user completed the word/phrase
                 }
+                /**** User guesses incorrectly ****/
                 else {
                     guessesLeft--;
                     console.log(chalk.red(`\nSorry that is wrong.\n`));
-                    //*******also show a line of already guessed letters
+                    console.log(chalk.cyan(`Guessed Letters: ${guessedChars}`));
+                    // shows a line of guessed chars
+                    console.log("");
                     console.log(chalk.magenta(newWord.wordRender()));
-                    if (guessesLeft != 0) {
-                        console.log(chalk.yellow(`\nYou have ${guessesLeft} guesses left! Don't waste them.\n`));
-                    }
-                    else {
-                        console.log(chalk.cyan(`GAMEOVER!`));
-                        //*******show what the correct word was
-                        return;
-                        // exits the enitre game
-                        //*******add another prompt to see if user wants to play again
-                    }
-                    
+                    // shows the underscores of word/phrase the user is trying to guess
+
+                        /**** User guesses incorrectly but still has more attempts left ****/
+                        if ( guessesLeft != 0 ) {
+                            console.log(chalk.yellow(`\nYou have ${guessesLeft} guesses left! Don't waste them.\n`));
+                            // tells user how many guesses they have left
+                        }
+                        /**** User guesses incorrectly and has no more attempts left ****/
+                        else {
+                            console.log("");
+                            console.log(chalk.blue(`GAMEOVER! The correct word/phrase was ${newWord}`));
+                            // shows what the correct word was
+                            return;
+                            // exits the enitre game
+                            //*******add another prompt to see if user wants to play again
+                        }
                     newWord.wordGuessed();
+                    // runs function to see if user completed the word/phrase
                 }
-                
+
                 if (guessesLeft > 0 && newWord.wordFound === false) {
                     promptUser();
                     // continues to prompt user to keep guessing if they have more than 0 guessesLeft
-                }
+                } 
                 else {
                     guessesLeft = 7;
                     // resets the guessesLeft 
+                    guessedChars = [];
+                    // resets the guessedChars
                     console.log(chalk.green(`You guessed the word/phrase correctly!\n`));
                     console.log(chalk.blue(`Here is a new word/phrase! See if you can get this one right!`));
 
@@ -85,10 +106,9 @@ function promptUser() {
                     //picks a new word for the user to guess
                 }
             })
-            .catch( err => {
-                console.log(err.message);
-            })
+                .catch( err => console.log(err.message))
     }
-} 
+}
 
 generateRandomWord();
+// invokes the function to start the game
